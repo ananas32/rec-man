e0742deec8bc027d106b7ce46e9f9ae5ec709858
Vitalii <porsche839@gmail.com>
2025-09-05 08:56:29 +0300

    add env config instead string value

--- php/src/config.php
+++ php/src/config.php
@@ -1,9 +1,12 @@
 <?php
 
-$host = 'db';
-$dbname = 'recman';
-$user = 'testuser';
-$pass = 'testpass';
+require_once __DIR__ . '/config/env.php';
+loadEnv(__DIR__ . '/.env');
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
